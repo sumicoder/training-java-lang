a10dc5c86e1be330d19d8a38ae54bab52e63b5c4
鶴浜弘嗣 <121598930+sumicoder@users.noreply.github.com>
2026-09-09 21:36:35 +0900

    キャスト

--- StuSample.java
+++ StuSample.java
@@ -57,15 +57,24 @@ public class StuSample {
     // PersonExtends studentExtendsConstructor3 = new PersonExtends("山田太郎", 123456);
     // studentExtendsConstructor3.display(); // 名前：山田太郎、学籍番号：123456
     // System.out.println("--------------------------------");
-    System.out.println("========== 学生の成績管理（final） ==========");
-    final int BASE_STU_NO = 100000;
-    PersonFinalExtends studentFinalExtends1 = new PersonFinalExtends("山田太郎", BASE_STU_NO);
-    studentFinalExtends1.display(); // 名前：山田太郎、学籍番号：100000
-    PersonFinalExtends studentFinalExtends2 = new PersonFinalExtends("佐藤花子", BASE_STU_NO + 1);
-    studentFinalExtends2.display(); // 名前：佐藤花子、学籍番号：100001
-    // BASE_STU_NO = 1000; // finalなので変更できないのでエラー
-    PersonFinalExtends studentFinalExtends3 = new PersonFinalExtends("鈴木次郎", BASE_STU_NO + 2);
-    studentFinalExtends3.display(); // 名前：鈴木次郎、学籍番号：100002
+    // System.out.println("========== 学生の成績管理（final） ==========");
+    // final int BASE_STU_NO = 100000;
+    // PersonFinalExtends studentFinalExtends1 = new PersonFinalExtends("山田太郎", BASE_STU_NO);
+    // studentFinalExtends1.display(); // 名前：山田太郎、学籍番号：100000
+    // PersonFinalExtends studentFinalExtends2 = new PersonFinalExtends("佐藤花子", BASE_STU_NO + 1);
+    // studentFinalExtends2.display(); // 名前：佐藤花子、学籍番号：100001
+    // // BASE_STU_NO = 1000; // finalなので変更できないのでエラー
+    // PersonFinalExtends studentFinalExtends3 = new PersonFinalExtends("鈴木次郎", BASE_STU_NO + 2);
+    // studentFinalExtends3.display(); // 名前：鈴木次郎、学籍番号：100002
+    // System.out.println("--------------------------------");
+    System.out.println("========== キャスト ==========");
+    PersonExtends personCast = new PersonExtends("山田太郎");
+    Person psnCast = personCast;
+    PersonExtends personCast2 = (PersonExtends) psnCast;
+    psnCast.display();
+    // psnCast.setStuNo(999999);
+    personCast2.setStuNo(999999);
+    personCast2.display();
     System.out.println("--------------------------------");
   }
 }
